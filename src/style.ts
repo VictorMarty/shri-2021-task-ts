d9da96d987ec9e875095bbd10e8a396e49ed651e
Victor Martynov <vicmart143@gmail.com>
2021-10-12 21:09:33 +0700

    commit2

--- src/style.ts
+++ src/style.ts
@@ -1,6 +1,10 @@
 import { color, TOptionsColor } from './colors';
 import { markdown, TOptionsMarkDown } from './md';
-export type TOptions = TOptionsColor | TOptionsMarkDown
+export type TOptions = {
+    [key: string]: string | boolean | string[]
+}
+
+// export type TOptions = TOptionsColor | TOptionsMarkDown
 export function style(text: string, options : TOptions): string {
     if (text.length === 0) {
         return text;
